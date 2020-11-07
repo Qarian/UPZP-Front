c14d9bfe73ad9e4ace27d7e58ef40fa5e65401bb
Иван Феофилактов <van199928@mail.ru>
2020-11-07 01:32:33 +0100

    Test Login UI created. Login Actions Tested.

---
+++ Assets/Scripts/LoginHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginHandler : MonoBehaviour
+{
+    private Action<string> passwordReceivers ;
+    private Action<string> usernameReceivers;
+    private Action loginReceivers;
+
+    public void addPasswordReceiver(Action<string> receiver) { passwordReceivers += receiver; }
+    public void addusernameReceivers(Action<string> receiver) { usernameReceivers += receiver; }
+    public void addLoginReceivers(Action receiver) { loginReceivers += receiver; }
+    public void OnPasswordUpdate(string password) {
+        if (passwordReceivers != null)
+        {
+            passwordReceivers.Invoke(password);
+
+        }
+        Debug.Log(password);
+    }
+    public void OnUsernameUpdate(string username)
+    {
+        if (usernameReceivers != null)
+        {
+            usernameReceivers.Invoke(username);
+            
+        }
+        Debug.Log(username);
+    }
+    public void LoginPress() {
+        Debug.Log("attempt to login in");
+        if (loginReceivers != null){
+            
+            loginReceivers.Invoke();
+
+        }
+    }
+}
