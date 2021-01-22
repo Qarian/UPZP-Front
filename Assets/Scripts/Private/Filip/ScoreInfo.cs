ace7a79fcc7d69a70a318a6e79324d0bc3607d90
pwr204507 <73247695+pwr204507@users.noreply.github.com>
2021-01-22 16:30:39 +0100

    Simple score info added

---
+++ Assets/Scripts/Private/Filip/ScoreInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class ScoreInfo : MonoBehaviour
+{
+    public List<int> scoreList;
+
+    public void Start()
+    {
+        scoreList = new List<int> { 0, 0, 0, 0, 0, 0 };
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+        List<Rect> rectObjList = new List<Rect>();
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        for (int i = 0; i < this.scoreList.Count; i++) {
+            rectObjList.Add(new Rect(20, i*20+20, 180, 90));
+            GUI.Box(rectObjList[i], "Team #" + i + " : " + scoreList[i]
+                , style);
+        }
+    }
+}
---
+++ Assets/Scripts/Private/Filip/ScoreTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTest : MonoBehaviour {
+
+    GameObject scoreReferenceObject;
+    ScoreInfo scoreReferenceScript;
+
+    void Start()
+    {
+        scoreReferenceObject = GameObject.Find("ScoreInfo");
+        scoreReferenceScript = scoreReferenceObject.GetComponent<ScoreInfo>();
+        scoreReferenceScript.scoreList[2] = 3;
+        scoreReferenceScript.scoreList = new List<int> { 4, 8, 15, 16, 23, 42 };
+
+    }
+    void Update()
+    {
+
+    }
+
+}
