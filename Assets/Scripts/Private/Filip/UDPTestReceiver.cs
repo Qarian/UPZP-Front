c8a2d88e3e960c3dd29c0b40a577b4389d1ebdbd
pwr204507 <73247695+pwr204507@users.noreply.github.com>
2021-01-22 11:39:03 +0100

    Custom UDP sender/receiver testing scripts

---
+++ Assets/Scripts/Private/Filip/UDPTestReceiver.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UDPTestReceiver : MonoBehaviour {
+
+    public int port;
+
+    Thread receiverThread;
+    UdpClient client;
+    
+    public string lastReceivedPacket = "";
+    public string allReceivedPackets = "";
+    
+    public void Start() {
+        init();
+    }
+    
+    private void init() {
+        port = 11100;
+        receiverThread = new Thread(
+            new ThreadStart(ReceiveData)) {
+            IsBackground = true
+        };
+        receiverThread.Start();
+    }
+    
+    private void ReceiveData() {
+        client = new UdpClient(port);
+        while (true) {
+            try {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref endPoint);
+                string text = Encoding.UTF8.GetString(data);
+                print(">> " + text);
+                lastReceivedPacket = text;
+                allReceivedPackets = allReceivedPackets + text;
+            }
+            catch (Exception err) {
+                print(err.ToString());
+            }
+        }
+    }
+
+    void OnDisable() {
+        if (receiverThread != null) {
+            receiverThread.Abort();
+        }
+        client.Close();
+    }
+    
+    void OnGUI() {
+        Rect rectObj = new Rect(40, 40, 190, 380);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "Receiver test:" +
+                         "\nIP: 127.0.0.1" +
+                         "\nPort: " + port + " \n" +
+                         "\nLast received: \n" + lastReceivedPacket // +
+                         // "\n\nAll received: \n" + allReceivedPackets
+                         , style);
+    }
+
+}
---
+++ Assets/Scripts/Private/Filip/UDPTestSender.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UDPTestSender : MonoBehaviour{
+    
+    private string IP;
+    public int port;
+
+    IPEndPoint endPoint;
+    UdpClient client;
+    
+    string message = "";
+    
+    public void Start() {
+        init();
+    }
+
+    public void init() {
+        
+        IP = "127.0.0.1";
+        port = 11100;
+        
+        endPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+        
+        print("Test data being sent to: " + IP + " : " + port);
+        //sendString("test" + "\n");
+
+    }
+    
+    private void sendString(string message) {
+        try {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            client.Send(data, data.Length, endPoint);
+        }   
+        catch (Exception ex) {
+            print(ex.ToString());
+        }
+    }
+    
+    void OnGUI() {
+        Rect rectObj = new Rect(40, 360, 190, 380);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "Sender test: " +
+                         "\nIP: 127.0.0.1" +
+                         "\nPort: " + port
+                         , style);
+        
+        message = GUI.TextField(new Rect(40, 420, 140, 20), message);
+        if (GUI.Button(new Rect(87, 445, 50, 25), "SEND")) {
+            sendString(message + "\n");
+        }
+    }
+
+}
