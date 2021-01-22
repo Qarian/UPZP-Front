57ad57e3cc92dab7635d0d5f97c927744102f48c
pwr204507 <73247695+pwr204507@users.noreply.github.com>
2021-01-22 13:56:53 +0100

    Collectable update/creation method added

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -5,6 +5,7 @@ using Networking;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Upzp.GameStatus;
 using Upzp.PlayerInput;
@@ -26,11 +27,49 @@ public class CharacterManager : MonoBehaviour
         }
     }
 
-    public void createCL(Collectable cl)
-    { 
-    
-    }
     Vector3 start_position;
+    public void createCL(List<Collectable> CLdata)
+    {
+
+        GameObject tempGameObject = null;
+        List<GameObject> currentCLs = GameObject.FindGameObjectsWithTag("CLPrefab").ToList();
+        int currentIndex = 0, countDifference = CLdata.Count - currentCLs.Count;
+
+
+        // extract new positions of all collectables
+        Vector3[] CLpositions = new Vector3[CLdata.Count];
+        for (int i = 0; i < CLdata.Count; i++)
+        {
+            CLpositions[i] = Conversions.GeoToWorldPosition(CLdata[i].position.x, CLdata[i].position.y, map.CenterMercator, map.WorldRelativeScale).ToVector3xz();
+        }
+
+        // instantiate new collectables if needed
+        if (countDifference > 0)
+        {
+            for (int i = 0; i < countDifference; i++)
+            {
+                currentCLs.Add(Instantiate(CLPrefab, CLpositions[i], Quaternion.identity));
+                currentIndex++;
+            }
+        }
+        // destroy unnecessary collectables
+        else if (countDifference < 0)
+        {
+            for (int i = (-countDifference); i > 0; i--)
+            {
+                tempGameObject = currentCLs[i - 1];
+                currentCLs.RemoveAt(i - 1);
+                tempGameObject.Destroy();
+            }
+        }
+
+        // update positions for the remaining collectables
+        for (int i = 0; i < (currentCLs.Count - currentIndex); i++)
+        {
+            currentCLs[i].transform.position = CLpositions[currentIndex + i];
+        }
+    }
+    
     public void createPC(PlayerData PCdata)
     {
         var pos = PCdata.position;
@@ -76,6 +115,9 @@ public class CharacterManager : MonoBehaviour
     internal void Initialize(GameStats gameStats)
 
     {
+
+        createCL(gameStats.cls);
+
         targets = new Dictionary<uint, Transform>();
 
         foreach (Dictionary<uint, PlayerData> team in gameStats.teams)
@@ -94,9 +136,6 @@ public class CharacterManager : MonoBehaviour
 
             }
         }
-
-
-
 
     }
 }
